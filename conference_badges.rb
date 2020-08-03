11b29d43ae5fb6e14f284b87ffb8898761957cb8
Yani Asari <yani.iman1@gmail.com>
2020-08-03 05:05:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,5 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-    printer = badges.size
-  puts "Hello, my name is #{attendees}."
-  end
+  "Hello, my name is #{attendees}."
 end 
