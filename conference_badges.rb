b882df8a1a6460586bc747051472dd979e7db8dd
Yani Asari <yani.iman1@gmail.com>
2020-08-03 06:12:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
 def badge_maker(name)
 "Hello, my name is #{name}."
 end
+
 def batch_badge_creator(attendees)
   new_array = [ ]
   attendees.each do |name|
@@ -8,8 +9,15 @@ def batch_badge_creator(attendees)
 end
 new_array
 end 
+
 def assign_rooms(attendees)
   attendees.each_with_index.map do |name,x|
     "Hello, #{name}! You'll be assigned to room #{x+1}!"
   end 
 end 
+
+def printer(attendees)
+  badges_and_room_assignments.each_line do |line|
+    puts line.chomp
+  end
+end
