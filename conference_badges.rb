9915ee4d27fb492fa0b82dce9dea534b509df96b
Yani Asari <yani.iman1@gmail.com>
2020-08-03 05:09:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees.each do |attendees|
+  badges.each do |attendees|
   "Hello, my name is #{attendee}."
 end
 end 
